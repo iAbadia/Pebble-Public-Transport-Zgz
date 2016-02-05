924d62ca6f3a14ef483ce0eba5fb87aefaa43286
Iñaki <i.abadiaosta@gmail.com>
2016-02-05 10:45:20 +0100

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -8,15 +8,14 @@ mkdir -p ~/pebble-dev
 touch ~/pebble-dev/ENABLE_ANALYTICS
 
 # Get the Pebble SDK and toolchain
-PEBBLE_SDK_VER=${PEBBLE_SDK#PebbleSDK-}
 if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
-  wget https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/pebble-sdk-${PEBBLE_SDK_VER}.tar.bz2 -O PebbleSDK-${PEBBLE_SDK_VER}.tar.bz2
+  wget https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/${PEBBLE_SDK}.tar.bz2 -O ${PEBBLE_SDK}.tar.bz2
   wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
 
   # Extract the SDK
-  tar jxvf PebbleSDK-${PEBBLE_SDK_VER}.tar.bz2 -C ~/pebble-dev/
+  tar jxf ${PEBBLE_SDK}.tar.bz2 -C ~/pebble-dev/
   # Extract the toolchain
-  tar zxvf arm-cs-tools-ubuntu-universal.tar.gz -C ~/pebble-dev/${PEBBLE_SDK}
+  tar zxf arm-cs-tools-ubuntu-universal.tar.gz -C ~/pebble-dev/${PEBBLE_SDK}
 
   # Install the Python library dependencies locally
   cd ~/pebble-dev/${PEBBLE_SDK}
