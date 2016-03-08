186678adbb6135cf22674f3fbc2f11ab41a975c3
Iñaki Abadia <i.abadiaosta@gmail.com>
2016-03-08 14:34:40 +0100

    removed build folder

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/home/inaki/.pebble-sdk/SDKs/current/sdk-core/pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'basalt', u'aplite']
